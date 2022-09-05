414c547952b15aa20b8bc9e77dfebd2a742e6e90
chinmay <mail2chinmaya.jena@gmail.com>
2022-09-05 07:20:21 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,44 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  distDir: "build",
+  webpack: (config, options) => {
+    config.experiments = config.experiments || {};
+    config.experiments.topLevelAwait = true;
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name: "fe3",
+        filename: "static/consumerFile.js",
+        remoteType: "var",
+        remotes: {
+          fe1: options.isServer
+            ? path.resolve("../fe1/build/remoteEntry.js")
+            : "fe1",
+          fe2: options.isServer
+            ? path.resolve("../fe1/build/remoteEntry_2.js")
+            : "fe2",
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+          {
+            "react-dom": {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+        ],
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
